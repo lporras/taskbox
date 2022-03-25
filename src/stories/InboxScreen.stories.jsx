902a214d59bdfb51a77a2b46e5dff7a5679d8b89
Luis Alfredo Porras Páez <lporras16@gmail.com>
2022-03-25 11:44:40 -0500

    using events to click on tasks on story

--- src/stories/InboxScreen.stories.jsx
+++ src/stories/InboxScreen.stories.jsx
@@ -8,6 +8,14 @@ import { MockedState } from './TaskList.stories';
 
 import { Provider } from 'react-redux';
 
+import {
+ fireEvent,
+ within,
+ waitFor,
+ waitForElementToBeRemoved
+} from '@storybook/testing-library';
+
+
 export default {
   component: InboxScreen,
   title: 'InboxScreen',
@@ -28,6 +36,19 @@ Default.parameters = {
       ),
     ],
   },
+};
+
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Waits for the component to transition from the loading state
+  await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
+  // Waits for the component to be updated based on the store
+  await waitFor(async () => {
+    // Simulates pinning the first task
+    await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+    // Simulates pinning the third task
+    await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+  });
 };
 
 export const Error = Template.bind({});
