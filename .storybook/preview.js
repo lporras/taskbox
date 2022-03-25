9c923d4d18d60a49399fa59c2a2a06bd31a6f7b5
Luis Alfredo Porras Páez <lporras16@gmail.com>
2022-03-25 09:55:55 -0500

    adding inboxScreen and stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,12 @@
 import '../src/index.css';
+// Registers the msw addon
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+// Initialize MSW
+initialize();
+
+// Provide the MSW addon decorator globally
+export const decorators = [mswDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
